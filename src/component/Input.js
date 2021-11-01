50ef0651a1707064a64eb5bd1cc9d843b1273839
yusshu <yushu@unnamed.team>
2021-11-01 09:27:15 -0500

    make sections collapse-able, fix input sizing

--- src/component/Input.js
+++ src/component/Input.js
@@ -2,9 +2,10 @@ export default function Input({ name, placeholders = []}) {
     return (
         <label className="flex flex-col px-4 text-sm">
             {name}
-            <div className="flex flex-row gap-2">
+            <div className="flex gap-2">
                 {placeholders.map(placeholder => (
-                    <input type="text" placeholder={placeholder} className="py-1 px-2 text-base bg-gray-300 outline-none border-none flex-1" />
+                    <input type="text" placeholder={placeholder}
+                           className="py-1 px-2 text-base bg-gray-300 outline-none border-none flex-1 w-1/2" />
                 ))}
             </div>
         </label>
--- src/component/Section.js
+++ src/component/Section.js
@@ -1,13 +1,17 @@
-import { FaChevronDown } from "react-icons/all";
+import {FaChevronDown, FaChevronUp} from "react-icons/all";
+import {useState} from "react";
 
 export default function Section({ children, name }) {
+    const [ visible, setVisible ] = useState(true);
     return (
         <section className="flex flex-col py-4 gap-2">
-            <div className="flex flex-row justify-between uppercase px-4">
+            <div className="flex flex-row justify-between uppercase px-4 cursor-pointer" onClick={() => setVisible(!visible)}>
                 <span>{name}</span>
-                <span><FaChevronDown /></span>
+                <span>{visible ? <FaChevronDown /> : <FaChevronUp />}</span>
             </div>
-            {children}
+            <div className={visible ? "block" : "hidden"}>
+                {children}
+            </div>
         </section>
     );
 }
